1b54269b0b0aa0893851226f521357897c994dbf
OliverH1337 <126427763+OliverH1337@users.noreply.github.com>
2023-05-17 20:01:51 +1000

    Add files via upload

--- Vis_2.js
+++ Vis_2.js
@@ -1,14 +1,11 @@
 
 function init() {
 
-    var w = 500;
-    var h = 500;
-    //var barPadding = 30;
-    var padding = 50;
+
 
 
     //reads in age csv
-    d3.csv("AGE_Group.csv").then(function(data) {
+    d3.csv("AGE_Group2.csv").then(function(data) {
         //changes string to number values
         data.forEach(d => {
             d.OBS_VALUE = +d.OBS_VALUE;; 
@@ -23,32 +20,38 @@ function init() {
     function barChart() {
         
 
+
+        var w = 1200;
+    var h = 800;
+    var padding = 50;
+
+
         var svg = d3.select("#chart")
         .append("svg")
         .attr("width", w)
         .attr("height", h); 
      
         
-        var margin = {top: 20, right: 30, bottom: 30, left: 40};
+        var margin = {top: 50, right: 40, bottom: 70, left: 200};
         var innerWidth = w - margin.left - margin.right;
         var innerHeight = h - margin.top - margin.bottom;
 
         //x scale linear is chosen to show linear representation for visualisation
         var xScale = d3.scaleLinear()
                 .domain([0, d3.max(dataset, d => d.OBS_VALUE)])
-                .range([margin.left, w - margin.right]);
+                .range([0, innerWidth]);
         
         //loads the range to consol testing purpose
         console.log(xScale.range());
 
         var xAxsis = d3.axisBottom()
-                        .ticks(6)
+                        .ticks(12)
                         .scale(xScale);
 
         //scale for the y value band is used so values are collected
         var yScale = d3.scaleBand()
                 .domain(dataset.map(d => d.age))
-                .range([ h - padding, padding ])
+                .range([ 0, innerHeight])
                 .padding(0.2);
         
         //loads domain to console testing purpose
@@ -56,42 +59,54 @@ function init() {
 
         var yAxsis = d3.axisLeft()
                         .ticks(20)
-                        .scale(yScale)
+                        .scale(yScale);
+        
+        
+            
 
         
 
+
+        //var g = svg.append("g")
+           // .attr("transform", "trasnlate( 200, 100)")
+        
         svg.selectAll("rect")
         .data(dataset)
         .enter()
         .append("rect")
+        .attr("transform", "translate(100,0)")
+      //  .attr("transform", "trasnlate( "+ (margin.left)+","+(margin.top)+")")
         .attr("y", d => yScale(d.age))
         .attr("width", d => xScale(d.OBS_VALUE))
-        .attr("height", yScale.bandwidth(yScale))
-        .attr("fill", "slateblue");
+        .attr("height", yScale.bandwidth())
+        .attr("fill", "red")
+        //.attr("transform", "translate(500,400)");
+        
         
         svg.append("g")
-            .attr("transform", "translate(-38, " + (h - margin.bottom ) +")") //pushes axis to bottom of page
+            .attr("transform", "translate(100, " + (h - margin.bottom) +")") //pushes axis to bottom of page
             .call(xAxsis)
             .append("text")
+            //.attr("transform", "rotate(-90)")
+            .attr("y", 30)
+            .attr("x", 550)
+            .attr("text-anchor", "end")
+            .attr("font-size", "15px")
+            .attr("fill", "white")
+            .text("immigrants") ;
+
+
+        svg.append("g")
+            .attr("transform", "translate (100,0)")
+            .call(yAxsis)
+            .append("text")
+            .attr("y", -50)
+            .attr("x", -250)
             .attr("transform", "rotate(-90)")
-            .attr("y", 50)
-            .attr("x", 250)
             .attr("text-anchor", "end")
             .attr("font-size", "15px")
             .attr("fill", "white")
             .text("Age Groups") ;
-;
-
-        svg.append("g")
-            .attr("transform", "translate (" +(padding)+",0)")
-            .call(yAxsis)
-            .append("text")
-            .attr("y", h )
-            .attr("x", w - 280)
-            .attr("text-anchor", "end")
-            .attr("font-size", "15px")
-            .attr("fill", "white")
-            .text("imigrants") ;
 
 
         d3.select("#year").on("change", function() {
@@ -101,9 +116,6 @@ function init() {
         
 
         })
-        
-
-
 
     }
 
